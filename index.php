09d6e956930abfdb8163d9ea26e52030bec8be0d
Lucianocanes <122166138+Lucianocanes@users.noreply.github.com>
2023-03-18 12:47:32 +0100

    Aggiornato index.php e modifiche allo style.css

--- index.php
+++ index.php
@@ -1,34 +1,32 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <link rel="stylesheet" type="text/css" href="style.php">
-    <style>
-  .test-box {
-    width: 10%;
-    height: 10%;
-    background-color: green;
-  }
-    </style>
-     <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contrada</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  </head>
+    </head>
   <body>
-    <body style="background-color:black;">
-
-    
-    <?php echo "<p style='color:White; text-align:center;  font-family:Lucida, monospace; border-radius: 65%; font-size:40px; border:5px red solid; background-color:black;'>Benvenuti nel sito della contrada</p>";?>
-    <br>
-    <img src="contrada.jpg" >
-    
-  
-  
-  
-  
-  
-  
-  
+  <body style="background-color:black;">
+    <div id="benvenuto">
+        <?php echo "<a href='https://discord.gg/YTH8VYyMTB' style='color:white;'> Benvenuti nel sito della contrada</a>"; ?>
+    </div>
+    <br> 
+    <div id="h1">
+        <h1>Seleziona che servizio vuoi utilizzare:</h1>
+    <div id="h2">
+      <div id="time-box">
+        <h2>Time converter </h2>
+      </div>
+      <div id="h2">
+        <div id="spotbox">
+          <h2>Spotify converter</h2>
+      </div>
+      <div id="h2">
+        <div id="meteo">
+          <h2>Meteo</h2>
+      </div>  
 
   </body>
 </html>
